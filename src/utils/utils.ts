bc57b79d1f8db0fe4dc20df26a62b36ac6d87300
roudanji <136449369+roudanji@users.noreply.github.com>
2023-11-23 10:53:52 +0800

    导出 Excel 函数封装上传

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -14,6 +14,23 @@ export const uniqueArray = (arr: Array<string | number>) =>
   Array.from(new Set(arr));
 
 /**
+ * @param { data }     二进制文件流
+ * @param { excelName }   导出的文件名字
+ */
+export const ExportExcel = (data: any, excelName: string) => {
+  const blob = new Blob([data], {
+    type: "application/vnd.ms-excel",
+  });
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement("a");
+  link.href = url;
+  link.download = `${excelName}`;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+};
+
+/**
  * @判断code码
  * @param { string }
  */
