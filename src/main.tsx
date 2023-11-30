8c6b6c4aea1809ae12ee2e68f6b63034ca63e5db
roudanji <136449369+roudanji@users.noreply.github.com>
2023-11-30 11:26:01 +0800

    message 轻提示最大显示数量

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import App from "@/App.tsx";
-import { ConfigProvider } from "antd";
+import { ConfigProvider, message } from "antd";
 import zhCN from "antd/lib/locale/zh_CN";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -9,6 +9,7 @@ import "./index.less";
 import "../mock/login.ts";
 import "../mock/userInfo.ts";
 
+message.config({ maxCount: 3 });
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <ConfigProvider locale={zhCN} componentSize="middle">
     <BrowserRouter>
