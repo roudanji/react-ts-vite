6fc8ca6138581022637eff10bf1240ba1cc6aa4a
roudanji <136449369+roudanji@users.noreply.github.com>
2023-11-30 09:33:41 +0800

    首页 card 修改

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -6,7 +6,7 @@ export default () => {
     <div className="home_box">
       <div className="home_left">
         <Space direction="vertical" size="middle" style={{ width: "100%" }}>
-          <Badge.Ribbon text="引导" color="cyan">
+          <Badge.Ribbon text="代码模式" color="magenta">
             <Card title="React Context 开发模式" size="small">
               <p>
                 可前往 scr / layouts / meunComponent || scr / pages / login 查看
@@ -16,7 +16,7 @@ export default () => {
             </Card>
           </Badge.Ribbon>
 
-          <Badge.Ribbon text="引导" color="cyan">
+          <Badge.Ribbon text="代码规范" color="cyan">
             <Card
               title="代码格式规范 Eslint / Prettier - Code formatter 插件"
               size="small"
@@ -29,15 +29,17 @@ export default () => {
             </Card>
           </Badge.Ribbon>
 
-          <Badge.Ribbon text="引导" color="cyan">
-            <Card title="axios 封装" size="small">
-              <p>src / utils / request.ts </p>
+          <Badge.Ribbon text="动态菜单" color="volcano">
+            <Card title="动态路由" size="small">
+              <p>Mock / userInfo / jurisdiction 字段</p>
               <br />
-              <p>125 行有具体调用示例</p>
+              <p>菜单配置在 layouts / menus.tsx 文件中</p>
+              <br />
+              <p>具体过滤方法是 findMenuDataByKey 依据 key 值过滤</p>
             </Card>
           </Badge.Ribbon>
 
-          <Badge.Ribbon text="引导" color="cyan">
+          <Badge.Ribbon text="全局组件样式" color="purple">
             <Card title="组件库主题  / 左侧菜单栏主题" size="small">
               <p>src / index.less </p>
               <br />
@@ -50,7 +52,7 @@ export default () => {
             </Card>
           </Badge.Ribbon>
 
-          <Badge.Ribbon text="引导" color="cyan">
+          <Badge.Ribbon text="导航栏 / Tabs" color="pink">
             <Card title="导航栏 / 面包屑" size="small">
               <p>使用的是 Tabs 组件实现</p>
               <br />
@@ -67,18 +69,16 @@ export default () => {
       </div>
       <div className="home_right">
         <Space direction="vertical" size="middle" style={{ width: "99%" }}>
-          <Badge.Ribbon text="引导" color="cyan">
-            <Card title="动态路由" size="small">
-              <p>Mock / userInfo / jurisdiction 字段</p>
+          <Badge.Ribbon text="封装" color="red">
+            <Card title="axios 封装" size="small">
+              <p>src / utils / request.ts </p>
               <br />
-              <p>菜单配置在 layouts / menus.tsx 文件中</p>
-              <br />
-              <p>具体过滤方法是 findMenuDataByKey 依据 key 值过滤</p>
+              <p>125 行有具体调用示例</p>
             </Card>
           </Badge.Ribbon>
 
-          <Badge.Ribbon text="引导" color="cyan">
-            <Card title="富文本编辑器" size="small">
+          <Badge.Ribbon text="封装" color="red">
+            <Card title="富文本编辑器封装" size="small">
               <p>借助的是 wangEditor 插件库</p>
               <br />
               <p>具体封装在 src / component / editor </p>
@@ -90,7 +90,7 @@ export default () => {
             </Card>
           </Badge.Ribbon>
 
-          <Badge.Ribbon text="引导" color="cyan">
+          <Badge.Ribbon text="封装" color="red">
             <Card title="message / notification 组件二封" size="small">
               <p>这两个组件进行了封装 可以节省大量重复性代码</p>
               <br />
@@ -100,7 +100,17 @@ export default () => {
             </Card>
           </Badge.Ribbon>
 
-          <Badge.Ribbon text="引导" color="cyan">
+          <Badge.Ribbon text="封装" color="red">
+            <Card title="导出 Excel 封装" size="small">
+              <p>src / utils / utils.ts</p>
+              <br />
+              <p>无采用插件借助 new Blob 实现生成下载链接</p>
+              <br />
+              <p>需要后端返回文件流导出</p>
+            </Card>
+          </Badge.Ribbon>
+
+          <Badge.Ribbon text="封装" color="red">
             <Card title="判断接口是否正常返回" size="small">
               <p>封装了映射函数建议整体项目使用 可以节省大量重复判断</p>
               <br />
